b3039d3dfde2d2eccdc77d4b0f7a85e87bd527eb
William Kim <73252368+wxkim@users.noreply.github.com>
2024-10-15 17:01:55 -0500

    board logic and selectors

---
+++ src/game.asm
@@ -0,0 +1,51 @@
+## Game Logic file
+
+
+.data
+
+exprs: .asciiz	"2x3", "3x4", "5x2", "5x3", 	# expression board 4 by 4 bank
+		"4x4", "2x7", "6x6", "4x5", 
+		"3x7", "8x5", "5x5", "5x6", 
+		"2x9", "1x7", "4x6", "5x7"
+		
+values: .asciiz " 6 ", "12 ", "10 ", "15 ",	# value board 4 by 4 bank
+		"16 ", "14 ", "36 ", "20 ",
+		"21 ", "40 ", "25 ", "30 ",	
+		"18 ", " 7 ", "24 ", "35 "
+
+generated: .space 128 					# 16 x 8 reserved mem for board creation
+randArray: .space 128 					# 16 x 8 reserved mem for second rng engine (placement)
+
+board: .asciiz 	" ? ", " ? ", " ? ", " ? ", 	# default board hidden
+		" ? ", " ? ", " ? ", " ? ", 
+		" ? ", " ? ", " ? ", " ? ", 
+		" ? ", " ? ", " ? ", " ? "
+
+usedAR: .word 	0,0,0,0,				# boolean array for used indices in rng (expr)
+		0,0,0,0,
+		0,0,0,0,
+		0,0,0,0
+
+usedRNG: .word 	0,0,0,0,				# boolean array for used indiced in rng (vals)
+		0,0,0,0,
+		0,0,0,0,
+		0,0,0,0
+		
+
+.text
+print:
+	la $t0, exprs
+	li $v0, 4
+	
+	la $a0, 0($t0)
+	syscall
+	
+	la $a0, 4($t0)
+	syscall
+	
+	la $a0, 8($t0)
+	syscall
+	
+	li $v0, 10
+	syscall
+	
--- src/main.asm
+++ src/main.asm
@@ -1,1 +1,14 @@
 # main file to compile from
+# CS2340 Long Project
+# Authors: Will Kim ||| wxk220007 ||| wkim@utdallas.edu
+
+# defined variables
+# create arrays
+# rng upper bound 16 (0 to 16-1) for index
+# find index on rng via sll 2
+# at index of RNG place 1 (bool array) in usedAR
+# first rng engine is for selecting from bank (8 times)
+# second rng engine for placement during board creation (8 times)
+
+
+

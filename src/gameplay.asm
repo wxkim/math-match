2b637b6cb76bafd07471816826a86a9532eb11b3
Henrique <sh10079979@gmail.com>
2024-11-15 17:20:59 -0600

    main game loop
    Created main game loop, fixed solveCard, and added exception handling for user input.

--- src/gameplay.asm
+++ src/gameplay.asm
@@ -2,6 +2,9 @@
 game_begin_message_string: .asciiz "Press OK to begin."
 game_end_message_string: .asciiz "Game over! Your time: "
 game_quit_message_string: .asciiz "You have quit the game. "
+user_out_of_bound: .asciiz "\nINDEX OUT OF BOUNDS. Range is (0-15)\n"
+user_error_same_index: .asciiz "\nINDEX WAS ALREADY INPUTTED. Please input two different indexes.\n"
+user_error_index_match: .asciiz "\nINDEX IS ALREADY MATCHED. Please input two indexes that have yet to be matched.\n"
 
 .text
 .globl game_start_popup
@@ -10,7 +13,6 @@ game_quit_message_string: .asciiz "You have quit the game. "
 .globl game_end_message_string
 .globl game_board_array_populate
 .globl game_start_function
-
 
 game_start_popup:
 	addi $sp $sp -4		#adjust stack pointer
@@ -29,9 +31,9 @@ game_end_popup:
 	jal find_current_time_ms
 	add $a1 $0 $0 # -> this is where time in integer goes; implement time first
 	syscall
-	li $v0 10
-	syscall ## GAME ENDS HERE
 	
+	li $v0 10
+	syscall
 ########################################################################################
 	
 game_board_array_populate:
@@ -154,11 +156,231 @@ return_from_gbap:
 ########################################################################################
 	
 game_start_function:
+	addi $sp $sp -24
+	sw $ra 0($sp)
+	sw $s0 4($sp) # first cards index
+	sw $s1 8($sp) # second cards index
+	sw $s2 12($sp) # first cards value
+	sw $s3 16($sp) # second cards value
+	sw $s4 20($sp) # size of display delay loop
 
-game_loop:
+	li $t4 0 # used for flipping cards
+	li $t5 0 # used for flipping cards
+	li $t6 0 # used for flipping cards
+	li $t7 0 # used for flipping cards
+	li $t9 0 # counts number of matches user makes
+game_loop_one:
+	li $s4 11500
+	# user wins game
+	beq $t9 8 game_win
+	
+	la $a0 board
+	jal printBoard
+	
+	# read first index value
+	jal read_user_input
+	bgt $a0 15 user_invalid_input
+	beq $a0 -1 user_invalid_input
 
+	# save index for later use
+	move $t5 $a0
+	move $s0 $a0
+	
+	# check if user has already matched at inputted index
+	jal check_prev_index
+	
+	# display flipped card
+	move $t5 $s0
+	jal clear_console
+	jal flip_card
+	la $a0 flippableBoard
+	jal printBoard
+	
+	# get first card's value
+	move $a0 $s0
+	jal solveCard
+	move $s2 $v0
+	
+game_loop_two:
+	# read second index value
+	jal read_user_input
+	bgt $a0 15 user_invalid_input
+	beq $a0 -1 user_invalid_input
+	beq $a0 $s0 user_same_index
+	
+	# save index for later use
+	move $t5 $a0
+	move $s1 $a0
+	
+	# check if user has already matched at inputted index
+	jal check_prev_index
+	
+	# display both flipped cards
+	move $t5 $s1
+	jal clear_console
+	jal flip_card
+	la $a0 flippableBoard
+	jal printBoard
+	
+	# get second card's value
+	move $a0 $s1
+	jal solveCard
+	move $s3 $v0
+	
+	beq $s3 $s2 match_success
+	bne $s3 $s2 match_fail
+	
+	# display new board 
+	jal clear_console
+	la $a0 board
+	jal printBoard
 
+	j game_loop_one
+	
+flip_card:
 
+	la $t4 randArray
+	sll $t5 $t5 2	# index *4 for offset
+	add $t6 $t4 $t5
+	lw $t7 0($t6)
+
+	la $t4 flippableBoard
+	add $t6 $t5 $t4
+	sw $t7 0($t6)
+	
+	jr $ra
+			
+match_success:
+	addi $t9 $t9 1 # increment match counter
+	jal match_success_sound
+	
+	# create new board with matched cards flipped over
+	la $t4 flippableBoard
+	sll $s0 $s0 2
+	add $t6 $s0 $t4
+	lw $t7 0($t6)
+	
+	la $t4 board
+	add $t6 $s0 $t4
+	sw $t7 0($t6)
+	
+	la $t4 flippableBoard
+	sll $s1 $s1 2
+	add $t6 $s1 $t4
+	lw $t7 0($t6)
+	
+	la $t4 board
+	add $t6 $s1 $t4
+	sw $t7 0($t6)
+	
+
+	jal clear_console
+
+	j game_loop_one	
+	
+match_fail:
+	jal match_fail_sound
+	
+	# resets flippable board with question marks in respective indexes
+	la $t4 board
+	sll $s0 $s0 2
+	add $t6 $t4 $s0
+	lw $t7 0($t6)
+	
+	la $t4 flippableBoard
+	add $t6 $t4 $s0
+	sw $t7 0($t6)
+	
+	la $t4 board
+	sll $s1 $s1 2
+	add $t6 $t4 $s1
+	lw $t7 0($t6)
+	
+	la $t4 flippableBoard
+	add $t6 $t4 $s1
+	sw $t7 0($t6)
+	
+	jal display_delay_buffer
+	jal clear_console
+	
+	j game_loop_one
+
+# a buffer so user has time to see incorrect match before clearing console	
+display_delay_buffer:
+	subi $s4 $s4 1
+	bnez $s4 display_delay_buffer
+	
+	jr $ra
+	
+# Exception handling if user inputs an index that was previously matched		
+check_prev_index:
+	# checks if there is a question mark at indexed card and makes user redo input if there isnt
+	la $t4 board
+	sll $t5 $t5 2
+	add $t6 $t4 $t5
+	
+	lbu $t7 1($t6)
+	bne $t7 '?' display_error_match
+	
+	jr $ra
+	
+# display error message for prev index error and jump back to game loop
+display_error_match:
+	jal clear_console
+	la $a0 user_error_index_match
+	li $v0 4
+	syscall
+	
+	j game_loop_one
+	
+# Exception handling if user inputs oob index	
+user_invalid_input:
+	jal clear_console
+	li $v0 4
+	la $a0 user_out_of_bound
+	syscall
+	
+	j game_loop_one
+	
+# Exception handling if user inputs the same index 
+user_same_index:
+	jal clear_console
+	li $v0 4
+	la $a0 user_error_same_index
+	syscall
+	
+	# Reset flippable board to get rid of the invalid input by user
+	la $t4 board
+	sll $s0 $s0 2
+	add $t6 $t4 $s0
+	lw $t7 0($t6)
+	
+	la $t4 flippableBoard
+	add $t6 $t4 $s0
+	sw $t7 0($t6)
+	
+	la $t4 board
+	sll $s1 $s1 2
+	add $t6 $t4 $s1
+	lw $t7 0($t6)
+	
+	la $t4 flippableBoard
+	add $t6 $t4 $s1
+	sw $t7 0($t6)
+	
+	j game_loop_one
+	
+# User won game return to main	
+game_win:
+	lw $ra 0($sp)
+	lw $s0 4($sp)
+	lw $s1 8($sp)
+	lw $s2 12($sp)
+	lw $s3 16($sp)
+	lw $s4 20($sp)
+	addi $sp $sp 24
+	
+	jr $ra
 
 	
 	
--- src/main.asm
+++ src/main.asm
@@ -1,52 +1,37 @@
 # Main file to compile from
 # CS2340 Long Project
 # Authors: Will Kim ||| wxk220007 ||| wkim@utdallas.edu
-# Teammate: Henrique Santos
+# Teammate: Henrique Santos || HGS230001 || hgs230001@utdallas.edu
 
 .data
 _main_msg_: .asciiz "In main (game loading)...\n"
 _main_msg_ingame_: .asciiz "\nPlaying game"
 
-.globl main 
-
 .text
+.globl main 
 main:
 	jal clear_console
 	li $v0 4
 	la $a0 _main_msg_
 	syscall
 	
-	#jal read_user_input	
+	# randomize board and populate randArray
+	jal game_board_array_populate
 	
-	#jal game_start_popup
+	# game start goes into game loop
+	jal game_start_popup
+	jal game_start_function
 	
-	#jal game_board_array_populate
+	# user won game 
+	jal game_win_sound
+	la $a0 board
+	jal printBoard
+	# jal game_end_popup have to implement time first
 	
-	#la $a0 randArray
-	#jal printBoard
-
-	
-	#jal match_success_sound
-	#jal match_fail_sound
-	#jal card_select_sound
-	#jal game_win_sound
-	
-	#jal clear_console
+	li $v0 10
+	syscall
 	
 	#li $v0 4
 	#la $a0 _main_msg_ingame_ 
 	#syscall
 	
-	
-
-	
-	#jal game_end_popup
-	
-	
-	
-	li $v0 10
-	syscall
-	
-
-
-
